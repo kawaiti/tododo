2601ddf178e0af3eb8d9bde3128af9b0f49103e8
kawaiti <kawaiti@gmail.com>
2010-09-14 00:49:30 +0900

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tododo::Application.config.secret_token = 'b87ac5248b53a526abd890d45fed0e8adbe5a687131dcfdf4594804d1dbbfc4bd0be3c8554603e87212faef7b2bfbf2a77c4148b20884b51f749a1ddd1f323f2'
